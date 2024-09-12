8e1caae9428b4c7affe89516199f3b0d8311eede
Tatyane Gonçalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-09-12 20:21:25 -0300

    Inicia menu da aplicacao

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-function start() {
+const { select } = require('@inquirer/prompts');
+
+
+let meta = {
+  value: "ler um livro por mês",
+  checked: false
+};
+
+
+let metas = [
+  meta,
+  {
+    value: "caminhar 20 minutos todos os dias",
+    checked: false
+  }
+];
+
+const start = async () => {
   while(true) {
-   let opcao = "sair"
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar"
+        },
+        {
+          name: "Listar metas",
+          value: "listar"
+        },
+        {
+          name: "Sair",
+          value: "sair"
+        }
+      ]
+    });
+
+
+
+   
 
    switch(opcao) {
     case "cadastrar":
@@ -10,6 +47,7 @@ function start() {
       console.log("vamos listar")
       break
     case "sair":
+      console.log("Até a próxima!")
       return
    }
   }
