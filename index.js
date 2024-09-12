6e157f8652d20eaee5e68cd7115d0046b2120c31
Tatyane Gonçalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-09-12 19:54:23 -0300

    Add Aula 1

---
+++ index.js
@@ -0,0 +1,18 @@
+function start() {
+  while(true) {
+   let opcao = "sair"
+
+   switch(opcao) {
+    case "cadastrar":
+      console.log("vamos cadastrar")
+      break
+    case "listar":
+      console.log("vamos listar")
+      break
+    case "sair":
+      return
+   }
+  }
+}
+
+start()
