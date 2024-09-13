3ce8d3efc3983add05a3dbf2c7429d36347501b5
Tatyane Gonçalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-09-13 09:41:36 -0300

    deletar metas

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: "Metas realizadas", 
+    message: realizadas.length + " Metas realizadas", 
     choices: [...realizadas]
   })
 }
@@ -81,6 +81,34 @@ const metasAbertas = async () => {
     message: abertas.length + " Metas abertas",
     choices: [...abertas]
   })
+}
+
+const deletarMetas = async () => {
+  const metasDesmarcadas = metas.map((meta) => {
+    return {
+      value: meta.value,
+      checked: false
+    }
+  })
+
+  const itensADeletar = await checkbox({
+    message: "Selecione item para deletar",
+    choices: [...metasDesmarcadas],
+    instructions: false,
+  })
+
+  if (itensADeletar.length == 0) {
+    console.log("Nenhum item para deletar!")
+    return
+  }
+
+  itensADeletar.forEach((item) => {
+    metas = metas.filter((meta) => {
+      return meta.value != item
+    })
+  })
+
+  console.log("Meta(s) deletada(s) com sucesso!")
 }
 
 const start = async () => {
@@ -103,6 +131,10 @@ const start = async () => {
         {
           name: "Metas abertas",
           value: "abertas"
+        },
+        {
+          name: "Deletar metas",
+          value: "deletar"
         },
         {
           name: "Sair",
@@ -128,6 +160,9 @@ const start = async () => {
       break
     case "abertas":
       await metasAbertas()
+      break
+    case "deletar":
+      await deletarMetas()
       break
     case "sair":
       console.log("Até a próxima!")
