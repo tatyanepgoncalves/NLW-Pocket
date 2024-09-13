eef8e4951ec890b188a2c0f78c4c2c5a7b7b431a
Tatyane Gonçalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-09-13 08:57:29 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -51,6 +51,22 @@ const listarMetas = async () => {
   console.log("Meta(s) marcadas como concluída(s)")
 }
 
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked
+  })
+
+  if (realizadas.length == 0) {
+    console.log("Não existem metas realizadas! :(") 
+    return
+  }
+
+  await select({
+    message: "Metas realizadas", 
+    choices: [...realizadas]
+  })
+}
+
 const start = async () => {
   while(true) {
     const opcao = await select({
@@ -63,6 +79,10 @@ const start = async () => {
         {
           name: "Listar metas",
           value: "listar"
+        },
+        {
+          name: "Metas realizadas",
+          value: "realizadas"
         },
         {
           name: "Sair",
@@ -82,6 +102,9 @@ const start = async () => {
       break
     case "listar":
       await listarMetas()
+      break
+    case "realizadas":
+      await metasRealizadas()
       break
     case "sair":
       console.log("Até a próxima!")
