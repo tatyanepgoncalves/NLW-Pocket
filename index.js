6fbcc559912de63756cb07181f2336360897c900
Tatyane Gonçalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-09-13 10:23:04 -0300

    Carregar e salvar metas com JSON

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const { select, input, checkbox } = require('@inquirer/prompts');
+const fs = require("fs").promises
 
 let mensagem = "Bem vindo ao App de Metas!"
 
-let meta = {
-  value: "ler um livro por mês",
-  checked: false
-};
+let metas 
 
-let metas = [ meta ];
+const carregarMetas = async () => {
+  try {
+    const dados = await fs.readFile("metas.json", "utf-8")
+    metas = JSON.parse(dados)
+  }
+  catch(erro) {
+    metas = []
+  }
+}
+
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite a meta:"})
@@ -136,9 +146,12 @@ const mostrarMensagem = () => {
 }
 
 const start = async () => {
+  await carregarMetas()
+
   while(true) {
     
     mostrarMensagem()
+    await salvarMetas( )
 
     const opcao = await select({
       message: "Menu >",
