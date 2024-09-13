cb795995207203a0847b6220eb1e0bcc7c3eedd1
Tatyane Gonçalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-09-13 09:17:01 -0300

    metas abertas

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const listarMetas = async () => {
   metas.forEach((m) => {
     m.checked = false
   })
-  
+
   if (respostas.length == 0) {
     console.log("Nenhuma meta selecionada!")
     return
@@ -68,6 +68,21 @@ const metasRealizadas = async () => {
   })
 }
 
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true
+  })
+
+  if(abertas.length == 0) {
+    console.log("Não existem metas abertas :)")
+  }
+
+  await select({
+    message: abertas.length + " Metas abertas",
+    choices: [...abertas]
+  })
+}
+
 const start = async () => {
   while(true) {
     const opcao = await select({
@@ -84,6 +99,10 @@ const start = async () => {
         {
           name: "Metas realizadas",
           value: "realizadas"
+        },
+        {
+          name: "Metas abertas",
+          value: "abertas"
         },
         {
           name: "Sair",
@@ -106,6 +125,9 @@ const start = async () => {
       break
     case "realizadas":
       await metasRealizadas()
+      break
+    case "abertas":
+      await metasAbertas()
       break
     case "sair":
       console.log("Até a próxima!")
